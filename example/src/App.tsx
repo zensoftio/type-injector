892fbc27c2a98a6671dbba54cca88a8d1c7ebc0a
Daniil Orlov <daniil.orlov@zensoft.io>
2019-02-26 09:57:45 +0600

    Updating tslint

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,23 +1,31 @@
-import * as React from 'react';
-import {injectAware} from 'react-dependency-injection'
-import './App.css';
-import logo from './logo.svg';
+import * as React from 'react'
+import {injectAware, injectProperty} from 'react-dependency-injection'
+import './App.css'
+import logo from './logo.svg'
+import Service from './service'
 
 @injectAware()
 class App extends React.Component {
+
+  @injectProperty('Service')
+  private service: Service
+
   public render() {
+
+    const greeting = this.service.value
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo'/>
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+        <p className='App-intro'>
+          {greeting}
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,11 +1,13 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+// @ts-ignore
+import Service from './service'
 
 ReactDOM.render(
-  <App />,
+  <App/>,
   document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+)
+registerServiceWorker()
